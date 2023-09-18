26604bd214891d1bc9ee43c8211641b01fad538f
Liam Adams <liam1540@googlemail.com>
2023-09-18 15:58:51 -0400

    Add function for gathering the grid values onto one process

--- shallow_water/state.py
+++ shallow_water/state.py
@@ -65,7 +65,47 @@ def create_par_field(locally_owned_field, geometry: ParGeometry):
     # locally_active_field = np.pad(locally_owned_field, (x_axis_padding, y_axis_padding))
     return ParField(locally_active_field, geometry)
 
+def gather_global_field(locally_owned_field, nxprocs, nyprocs, root, rank, mpi4py_comm):
+    '''Gather the distributed blocks of a field into a single 2D array on `rank == root`.
+    Warning: one must ensure that the communicator argument, `mpi4py_comm`, is a communicator that is not used with any mpi4jax routines, 
+    but strictly only with mpi4py routines.'''
 
+    assert locally_owned_field.ndim == 2
+
+    # Collect local array shapes
+    sendshapes = np.array(mpi4py_comm.gather(np.shape(locally_owned_field), root))
+
+    # Compute size of each shape
+    if rank == root:
+        assert np.size(sendshapes, axis=0) == (nxprocs * nyprocs)
+        shapesize = list(map(lambda shape: shape[0] * shape[1], [sendshapes[i,:] for i in range(np.size(sendshapes, axis=0))]))
+        sendcounts = np.array(shapesize)
+        sendcounts_prefix_sum = np.concatenate(([0], np.cumsum(sendcounts)))
+    else:
+        sendcounts = 0
+
+    # Prepare the recieve buffer
+    if rank == root:
+        global_field = np.empty(sum(sendcounts), dtype=locally_owned_field.dtype)
+    else:
+        global_field = None
+
+    # Gather the data
+    mpi4py_comm.Gatherv(sendbuf=locally_owned_field, recvbuf=(global_field, sendcounts), root=root)
+    if rank == root:
+        blocks = []
+        for i in range(nxprocs):
+            block_column = []
+            for j in range(nyprocs):
+                index = coord_to_index_xy_order(Vec2(nxprocs, nyprocs), Vec2(i, j))
+                block_start = sendcounts_prefix_sum[index]
+                block_end = sendcounts_prefix_sum[index + 1]
+                block_column.append(np.reshape(global_field[block_start:block_end], (sendshapes[index,0], sendshapes[index,1])))
+            blocks.append(block_column)
+
+        return np.block(blocks)
+
+    return np.empty((1,))
 
 # def flatten_geometry(geometry):
 #   flat_content = (geometry.nx, geometry.ny,
---
+++ tests/test_state_gather_field.py
@@ -0,0 +1,53 @@
+import pytest
+
+import jax.numpy as jnp
+import numpy as np
+
+from shallow_water.geometry import create_par_geometry, RectangularDomain, get_locally_owned_range
+from shallow_water.state import create_par_field, gather_global_field
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+root = 0
+
+
+def test_gather_global_field():
+    assert size == 4
+    
+    domain = RectangularDomain(9,9)
+
+    geometry = create_par_geometry(comm, rank, size, domain)
+    locally_owned_field = (rank + 1) * jnp.ones((geometry.locally_owned_extent_x,geometry.locally_owned_extent_y), dtype=jnp.float32)
+
+    if rank == 0:
+        locally_owned_field = locally_owned_field.at[0:2, 0].set(-2)
+        locally_owned_field = locally_owned_field.at[2:4, 0].set(-1)
+    if rank == 2:
+        locally_owned_field = locally_owned_field.at[0:2, 0].set(-4)
+        locally_owned_field = locally_owned_field.at[2:4, 0].set(-3)
+
+    field = create_par_field(locally_owned_field, geometry)
+
+    start, end = get_locally_owned_range(geometry)
+    locally_owned_field = np.array(field.value[start.x:end.x,start.y:end.y])
+    
+    global_field = gather_global_field(locally_owned_field, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, comm)
+
+    if rank == root:
+        if size == 4:
+            hypo_block_0 = 1 * jnp.ones((4,4), dtype=jnp.float32)
+            hypo_block_0 = hypo_block_0.at[0:2, 0].set(-2)
+            hypo_block_0 = hypo_block_0.at[2:4, 0].set(-1)
+            hypo_block_1 = 2 * jnp.ones((5,4), dtype=jnp.float32)
+            hypo_block_2 = 3 * jnp.ones((4,5), dtype=jnp.float32)
+            hypo_block_2 = hypo_block_2.at[0:2, 0].set(-4)
+            hypo_block_2 = hypo_block_2.at[2:4, 0].set(-3)
+            hypo_block_3 = 4 * jnp.ones((5,5), dtype=jnp.float32)
+
+            assert np.array_equal(global_field[0:4,0:4], hypo_block_0)
+            assert np.array_equal(global_field[4:9,0:4], hypo_block_1)
+            assert np.array_equal(global_field[0:4,4:9], hypo_block_2)
+            assert np.array_equal(global_field[4:9,4:9], hypo_block_3)
