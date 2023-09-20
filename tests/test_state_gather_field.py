3bbbacf71977e91dc17d95e23f424abec90ca2ee
Liam Adams <liam1540@googlemail.com>
2023-09-20 12:56:11 -0400

    Update test to use new helper functions

--- tests/test_state_gather_field.py
+++ tests/test_state_gather_field.py
@@ -3,8 +3,8 @@ import pytest
 import jax.numpy as jnp
 import numpy as np
 
-from shallow_water.geometry import create_par_geometry, RectangularDomain, get_locally_owned_range
-from shallow_water.state import create_par_field, gather_global_field
+from shallow_water.geometry import create_par_geometry, RectangularDomain, get_locally_owned_range, at_locally_owned
+from shallow_water.state import create_local_field_ones, gather_global_field
 from shallow_water.runtime_context import mpi4py_comm
 
 rank = mpi4py_comm.Get_rank()
@@ -18,21 +18,19 @@ def test_gather_global_field():
     domain = RectangularDomain(9,9)
 
     geometry = create_par_geometry(rank, size, domain)
-    locally_owned_field = (rank + 1) * jnp.ones((geometry.locally_owned_extent_x,geometry.locally_owned_extent_y), dtype=jnp.float32)
+    field = (rank + 1) * create_local_field_ones(geometry, jnp.float32)
+    start, end = get_locally_owned_range(geometry)
 
     if rank == 0:
-        locally_owned_field = locally_owned_field.at[0:2, 0].set(-2)
-        locally_owned_field = locally_owned_field.at[2:4, 0].set(-1)
+        field = field.at[start.x:start.x+2, start.y].set(-2)
+        field = field.at[start.x+2:start.x+4, start.y].set(-1)
     if rank == 2:
-        locally_owned_field = locally_owned_field.at[0:2, 0].set(-4)
-        locally_owned_field = locally_owned_field.at[2:4, 0].set(-3)
-
-    field = create_par_field(locally_owned_field, geometry)
-
-    start, end = get_locally_owned_range(geometry)
-    locally_owned_field = np.array(field.value[start.x:end.x,start.y:end.y])
+        field = field.at[start.x:start.x+2, start.y].set(-4)
+        field = field.at[start.x+2:start.x+4, start.y].set(-3)
+                                         
+    locally_owned_field = np.array(field[at_locally_owned(geometry)])
     
-    global_field = gather_global_field(locally_owned_field, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, comm)
+    global_field = gather_global_field(locally_owned_field, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
 
     if rank == root:
         if size == 4:
