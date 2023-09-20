11aa77f82a89cc00db0a5db22bfd1c40e43603d3
Liam Adams <liam1540@googlemail.com>
2023-09-20 13:24:53 -0400

    Add new model regression test

--- tests/test_model.py
+++ tests/test_model.py
@@ -87,74 +87,60 @@ def test_model_2():
         assert np.all(np.equal(v_global, [0.0]))
         assert np.all(np.equal(h_global, [10.0]))
 
-# def test_model_3():
+def test_model_3():
+    assert size == 4
 
-#     xmax = ymax = 10000.0
-#     nx = ny = 11
-#     dx = dy = xmax / (nx - 1.0)
-#     g = 9.81
-#     dt = 0.68 * dx / sqrt(g * 5030.0)
-#     num_steps = 1
+    xmax = ymax = 10000.0
+    nx = ny = 11
+    dx = dy = xmax / (nx - 1.0)
+    g = 9.81
+    dt = 0.68 * dx / sqrt(g * 5030.0)
+    num_steps = 100
 
-#     domain = RectangularDomain(nx, ny)
-#     geometry = create_par_geometry(mpi4jax_comm, rank, size, domain)
-#     locally_owned_field = 0 * jnp.ones((geometry.locally_owned_extent_x,geometry.locally_owned_extent_y), dtype=jnp.float64)
-#     zero_field = create_par_field(locally_owned_field, geometry)
+    domain = RectangularDomain(nx, ny)
+    geometry = create_par_geometry(rank, size, domain)
+    zero_field = create_local_field_zeros(geometry, jnp.float64)
 
-#     u = jnp.copy(zero_field.value)
-#     v = jnp.copy(zero_field.value)
-#     h = jnp.copy(zero_field.value)
-#     b = jnp.copy(zero_field.value)
+    u = jnp.copy(zero_field)
+    v = jnp.copy(zero_field)
+    h = jnp.copy(zero_field)
+    b = jnp.copy(zero_field)
 
-#     # Create tsunami pluse IC
-#     xmid = xmax / 2.0
-#     ymid = ymax / 2.0
-#     sigma = floor(xmax / 20.0)
-#     start, end = get_locally_owned_range(geometry)
-#     h_global = np.zeros((nx,ny), dtype=jnp.float64)
-#     for j in range(ny):
-#         for i in range(nx):
-#             dsqr = ((i) * dx - xmid)**2 + ((j) * dy - ymid)**2
-#             h_global[i,j] = (5000.0 + (5030.0 - 5000.0) * exp(-dsqr / sigma**2))
+    # Create tsunami pluse IC
+    xmid = xmax / 2.0
+    ymid = ymax / 2.0
+    sigma = floor(xmax / 20.0)
+    h_global = np.zeros((nx,ny), dtype=jnp.float64)
+    for j in range(ny):
+        for i in range(nx):
+            dsqr = ((i) * dx - xmid)**2 + ((j) * dy - ymid)**2
+            h_global[i,j] = (5000.0 + (5030.0 - 5000.0) * exp(-dsqr / sigma**2))
+    if rank == 0:
+        h = h.at[at_locally_owned(geometry)].set(h_global[0:5, 0:5])
+    if rank == 1:
+        h = h.at[at_locally_owned(geometry)].set(h_global[5:11, 0:5])
+    if rank == 2:
+        h = h.at[at_locally_owned(geometry)].set(h_global[0:5, 5:11])
+    if rank == 3:
+        h = h.at[at_locally_owned(geometry)].set(h_global[5:11, 5:11])
 
-#     if rank == 0:
-#         h = h.at[start.x:end.x, start.y:end.y].set(h_global[0:5, 0:5])
-#     if rank == 1:
-#         h = h.at[start.x:end.x, start.y:end.y].set(h_global[5:11, 0:5])
-#     if rank == 2:
-#         h = h.at[start.x:end.x, start.y:end.y].set(h_global[0:5, 5:11])
-#     if rank == 3:
-#         h = h.at[start.x:end.x, start.y:end.y].set(h_global[5:11, 5:11])
-
-#     state = State(u,v,h,1.0,geometry)
-
-#     start, end = get_locally_owned_range(geometry)
-
-#     locally_owned_field = np.array(state.h[start.x:end.x,start.y:end.y])
-#     visualize_locally_owned_field("ic.png", locally_owned_field, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
-
-#     new_state = advance_model_n_steps(state, b, num_steps, dt, dx, dy)
-
-#     locally_owned_field = np.array(new_state.h[start.x:end.x,start.y:end.y])
-#     visualize_locally_owned_field("step{}.png".format(num_steps), locally_owned_field, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
-
-#     u_locally_owned = np.array(state.u[start.x:end.x,start.y:end.y])
-#     v_locally_owned = np.array(state.v[start.x:end.x,start.y:end.y])
-#     h_locally_owned = np.array(state.h[start.x:end.x,start.y:end.y])
-    
-#     u_global = gather_global_field(u_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
-#     v_global = gather_global_field(v_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
-#     h_global = gather_global_field(h_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
-
-#     rms_u = sqrt(np.sum(u_global * u_global) / (nx * ny))
-#     rms_v = sqrt(np.sum(v_global * v_global) / (nx * ny))
-#     rms_h = sqrt(np.sum(h_global * h_global) / (nx * ny))
+    new_u, new_v, new_h = advance_model_n_steps(u, v, h, 1.0, geometry, b, num_steps, dt, dx, dy)
 
 
+    u_locally_owned = np.array(new_u[at_locally_owned(geometry)])
+    v_locally_owned = np.array(new_v[at_locally_owned(geometry)])
+    h_locally_owned = np.array(new_h[at_locally_owned(geometry)])
+    u_global = gather_global_field(u_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
+    v_global = gather_global_field(v_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
+    h_global = gather_global_field(h_locally_owned, geometry.pg_info.nxprocs, geometry.pg_info.nyprocs, root, rank, mpi4py_comm)
 
-#     if rank == root:
-#         print('rank({}): rms_u = {}, rms_v = {}, rms_h = {}'.format(rank, rms_u, rms_v, rms_h))
-#         assert abs(rms_u - 0.00161019683016338) < 10.e-12
-#         assert abs(rms_v - 0.00161019683016338) < 10.e-12
-#         assert abs(rms_h - 5000.37196249264) < 10.e-12
+    if rank == root:
+        rms_u = sqrt(np.sum(u_global ** 2) / (nx * ny))
+        rms_v = sqrt(np.sum(v_global ** 2) / (nx * ny))
+        rms_h = sqrt(np.sum(h_global ** 2) / (nx * ny))
+
+        print('rms_u = {}, rms_v = {}, rms_h = {}'.format(rank, rms_u, rms_v, rms_h))
+        assert abs(rms_u - 0.0016110910527818616) < 10.e-15
+        assert abs(rms_v - 0.0016110910527825017) < 10.e-15
+        assert abs(rms_h - 5000.371968791809) < 10.e-15
 
