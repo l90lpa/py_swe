ea2624152dcc61978fa659aca2c4d26d64414daa
Liam Adams <liam1540@googlemail.com>
2023-10-27 15:38:22 -0400

    Change variable names.

--- shallow_water/adm.py
+++ shallow_water/adm.py
@@ -5,25 +5,25 @@ import jax.numpy as jnp
 from .model import shallow_water_model
 
 
-def shallow_water_model_adm(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
+def shallow_water_model_adm(s, Ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
 
     def sw_model(s, token):
         return shallow_water_model(s, geometry, comm_wrapped, b, n_steps, dt, dx, dy, token)
     
     @jit
-    def sw_model_vjp_jit(x, token, Dx, Dtoken):
+    def sw_model_vjp_jit(x, token, Dy, Dtoken):
         primals, sw_model_vjp = vjp(sw_model, x, token)
-        cotangents = sw_model_vjp((Dx, Dtoken))
+        cotangents = sw_model_vjp((Dy, Dtoken))
         return primals, cotangents
 
     tok = jnp.empty((1,))
     Dtok = jnp.empty((1,))
-    (y, _), (Dy, _) = sw_model_vjp_jit(s, tok, s_t, Dtok)
+    (y, _), (Dx, _) = sw_model_vjp_jit(s, tok, Ds, Dtok)
     
-    return y, Dy
+    return y, Dx
 
 
 # This is a convience wrapper
-def advance_adm_n_steps(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
-    primals, tangents = shallow_water_model_adm(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy)
-    return primals, tangents
+def advance_adm_n_steps(s, Ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
+    primals, cotangents = shallow_water_model_adm(s, Ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy)
+    return primals, cotangents
--- shallow_water/tlm.py
+++ shallow_water/tlm.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 from .model import shallow_water_model
 
 
-def shallow_water_model_tlm(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
+def shallow_water_model_tlm(s, ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
 
     def sw_model(s, token):
         return shallow_water_model(s, geometry, comm_wrapped, b, n_steps, dt, dx, dy, token)
@@ -16,12 +16,12 @@ def shallow_water_model_tlm(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, 
 
     tok = jnp.empty((1,))
     dtok = jnp.empty((1,))
-    (y, _), (dy, _) = sw_model_jvp_jit(s, tok, s_t, dtok)
+    (y, _), (dy, _) = sw_model_jvp_jit(s, tok, ds, dtok)
     
     return y, dy
 
 
 # This is a convience wrapper
-def advance_tlm_n_steps(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
-    primals, tangents = shallow_water_model_tlm(s, s_t, geometry, comm_wrapped, b, n_steps, dt, dx, dy)
+def advance_tlm_n_steps(s, ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy):
+    primals, tangents = shallow_water_model_tlm(s, ds, geometry, comm_wrapped, b, n_steps, dt, dx, dy)
     return primals, tangents
