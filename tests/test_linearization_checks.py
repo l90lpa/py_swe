bceb9de02334d5d259ce1eac24afeddef6b19178
Liam Adams <liam1540@googlemail.com>
2023-10-27 17:05:15 -0400

    Make linearization tests pytest compatible.

--- tests/test_linearization_checks.py
+++ tests/test_linearization_checks.py
@@ -10,55 +10,56 @@ config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 
 
-if __name__ == "__main__":
+shape = (3,3)
+dtype = jnp.float64
+rng = np.random.default_rng(12345)
 
-    shape = (3,3)
-    dtype = jnp.float64
-    rng = np.random.default_rng(12345)
+## argument generators
+def primalArg():
+    return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
 
-    ## argument generators
-    def primalArg():
-        return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
+def tangentArg():
+    return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
 
-    def tangentArg():
-        return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
-    
-    def cotangentArg():
-        return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
-    
-    ## vector field operations for the arguments
-    def scale(a, x):
-        return a * x
-    
-    def add(x, y):
-        return x + y
-            
-    def dot(x,y):
-        dot_ = jnp.sum(x * y)
-        return dot_
-    
-    def norm(x):
-        return jnp.sqrt(dot(x,x))
+def cotangentArg():
+    return jnp.array(rng.normal(0.0, 1.0, shape), dtype=dtype)
 
-    ## test function and its linearizations
-    def identity(s):
-        return s
-    
-    def identity_tlm(s, ds):
-        return identity(s), ds
-    
-    def identity_adm(s, ds):
-        return identity(s), ds
-    
+## vector field operations for the arguments
+def scale(a, x):
+    return a * x
 
-    print("Test TLM Linearity:")
+def add(x, y):
+    return x + y
+        
+def dot(x,y):
+    dot_ = jnp.sum(x * y)
+    return dot_
+
+def norm(x):
+    return jnp.sqrt(dot(x,x))
+
+## test function and its linearizations
+def identity(s):
+    return s
+
+def identity_tlm(s, ds):
+    return identity(s), ds
+
+def identity_adm(s, ds):
+    return identity(s), ds
+
+
+def test_tlm_linearity():
     success, absolute_error = lc.testTLMLinearity(identity_tlm, primalArg, tangentArg, scale, norm, 1.0e-15)
-    print("success = ", success, ", absolute_error = ", absolute_error)
+    print(f"TLM Linearity Test: success={success}, absolute error={absolute_error}")
+    assert success
 
-    print("Test TLM Approximation:")
+def test_tlm_approx():
     success, relative_error = lc.testTLMApprox(identity, identity_tlm, primalArg, tangentArg, scale, add, norm, 1.0e-15)
-    print("success = ", success, ", relative error = ", relative_error)
+    print(f"TLM Approximation Test: success={success}, relative error={relative_error}")
+    assert success
 
-    print("Test ADM Approximation:")
+def test_spectral_theorem():
     success, absolute_error = lc.testSpectralTheorem(identity_tlm, identity_adm, primalArg, tangentArg, cotangentArg, dot, 1.0e-15)
-    print("success = ", success, ", absolute_error = ", absolute_error)
+    print(f"Spectral Theorem (\"Dot Product\") Test: success={success}, absolute error={absolute_error}")
+    assert success
